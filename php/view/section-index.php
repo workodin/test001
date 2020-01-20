2ea180d3be64350815272685df3be45ee95cdb35
test1 <test1@mail.me>
2020-01-21 00:54:07 +0100

    ajout lorem

--- php/view/section-index.php
+++ php/view/section-index.php
@@ -3,6 +3,7 @@
 
     <section>
         <h3>formulaire de contact</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus alias, sit, ipsa commodi omnis, cupiditate nihil blanditiis cumque quasi est delectus laborum soluta. Quaerat nesciunt eum dolorem veniam, sed sapiente.
     </section>
     <section>
         <img src="assets/images/photo.jpg" alt="photo">
