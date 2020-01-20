a11f3f24f24652b9a768c1bbefd1bbfaff51e83e
test1 <test1@mail.me>
2020-01-20 19:34:33 +0100

    debug css et js

--- 404.php
+++ 404.php
@@ -15,6 +15,17 @@ $master5    = md5("$dirname");
 $targetFile = __DIR__ . "/$dirname/$filename.$extension";
 if (is_file($targetFile))
 {
+    status_header(200);
+    switch($extension)
+    {
+        case "css":
+            header("Content-Type: text/css");
+            break;
+        case "css":
+                header("Content-Type: application/javascript");
+            break;
+        }
+
     if ("php" == $extension)
     {
         include $targetFile;
--- php/view/footer.php
+++ php/view/footer.php
@@ -3,5 +3,7 @@
         <footer>
             <p>tous droits réservés &copy;2020</p>
         </footer>
+
+        <script src="assets/js/site.js"></script>
     </body>
 </html>
