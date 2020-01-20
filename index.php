7cc6f66d156639675d450b17145723ba723f5450
test1 <test1@mail.me>
2020-01-20 19:13:55 +0100

    decoupage php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?php echo date("H:i:s") ?></h1>
-</body>
-</html>
+<?php
+
+require_once "php/view/header.php";
+require_once "php/view/section-index.php";
+require_once "php/view/footer.php";
---
+++ php/view/section-index.php
@@ -0,0 +1,2 @@
+
+    <h1><?php echo date("H:i:s") ?></h1>
