d9b31053884cf69c6b5dd4d84f492dca6aa04d24
test1 <test1@mail.me>
2020-01-20 20:28:46 +0100

    ajout verif capability WP

--- 404.php
+++ 404.php
@@ -12,41 +12,47 @@ extract(pathinfo($path));
 $dirname = trim($dirname, "/");
 $master5    = md5("$dirname");
 
-$targetFile = __DIR__ . "/$dirname/$filename.$extension";
-if (is_file($targetFile))
-{
-    status_header(200);
-    
-    $extension = strtolower($extension);
-    switch($extension)
-    {
-        case "css":
-            header("Content-Type: text/css");
-            break;
-        case "js":
-                header("Content-Type: application/javascript");
-            break;
-        case "php":
-                header("Content-Type: text/html");
-            break;
-        default:
-            $mimeType = mime_content_type($targetFile);
-            header("Content-Type: $mimeType");
-        }
+// SECURITY
+$gitCapability = "read";
 
-    if ("php" == $extension)
+if (($gitCapability != "") && current_user_can($gitCapability) ) 
+{
+    $targetFile = __DIR__ . "/$dirname/$filename.$extension";
+    if (is_file($targetFile))
     {
-        // protection contre modif fichiers
-        ini_set("open_basedir", "/tmp:" .__DIR__ . "/$dirname");
-        include $targetFile;
+        status_header(200);
+        
+        $extension = strtolower($extension);
+        switch($extension)
+        {
+            case "css":
+                header("Content-Type: text/css");
+                break;
+            case "js":
+                    header("Content-Type: application/javascript");
+                break;
+            case "php":
+                    header("Content-Type: text/html");
+                break;
+            default:
+                $mimeType = mime_content_type($targetFile);
+                header("Content-Type: $mimeType");
+            }
+
+        if ("php" == $extension)
+        {
+            // protection contre modif fichiers
+            ini_set("open_basedir", "/tmp:" .__DIR__ . "/$dirname");
+            include $targetFile;
+        }
+        else
+        {
+            readfile($targetFile);
+        }
     }
     else
     {
-        readfile($targetFile);
+        echo "$targetFile";
     }
-}
-else
-{
-    echo "$targetFile";
-}
 
+}
