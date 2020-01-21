c1e0787900f1e8af3ca1947d99ad3556c2496100
workodin <hello@workodin.com>
2020-01-21 03:00:56 +0100

    gitauto.sh

--- gitauto.sh
+++ gitauto.sh
@@ -10,7 +10,7 @@ if [ -z "$listmodif" ]
 then
     listfile=`git status --porcelain | cut -c4-`
 
-    if  [ -e "$lisfile"]
+    if  [ -n "$lisfile"]
     then
         git add -A 
         git commit -a -m "$listfile"
