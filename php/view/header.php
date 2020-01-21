9124b0a4a3b119a7f9389022b995a45ccaab515e
workodin <hello@workodin.com>
2020-01-21 04:23:34 +0100

    php/view/header.php

--- php/view/header.php
+++ php/view/header.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/site.css">
+    <meta name="description" content="mon site vitrine">
     <title>Site Vitrine</title>
+    <link rel="stylesheet" href="assets/css/site.css">
 </head>
 <body>
     <header>
