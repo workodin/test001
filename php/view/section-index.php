8600e57893e6d0c9ee772e8fa05905cef3c7e2d4
workodin <hello@workodin.com>
2020-01-21 15:52:27 +0100

    php/view/section-index.php

--- php/view/section-index.php
+++ php/view/section-index.php
@@ -1,3 +1,18 @@
+<section>
+        <h3>PRESENTATION</h3>
+        <div class="ligne x2col">
+            <article>
+                <h4>titre4</h4>
+                <div>colonne</div>
+                <img src="assets/images/photo.jpg" alt="photo">
+            </article>
+            <article>
+                <h4>titre4</h4>
+                <div>colonne</div>
+                <img src="assets/images/photo.jpg" alt="photo">
+            </article>
+        </div>
+    </section>
 
 
     <section>
