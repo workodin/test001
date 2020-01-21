b7fb678a5802282974e08355eb10454d604053a2
workodin <hello@workodin.com>
2020-01-21 15:31:36 +0100

    git-autopush.sh php/view/section-news.php

--- git-autopush.sh
+++ git-autopush.sh
@@ -1,11 +1,14 @@
 
+# usage
+# watch -n5 ./git-autopush.sh
 date +"%F %T"
 
 # cherche les fichiers modifiés dans la dernière minute
 listmodif=`find -not -path "*/.git/*" -type f -mmin 1`
 echo $listmodif
 
-if [ -n "$listmodif" ]
+# aucun fichier modifié depuis une minute
+if [ -z "$listmodif" ]
 then
     listfile=`git status --porcelain | cut -c4-`
 
--- php/view/section-news.php
+++ php/view/section-news.php
@@ -1,4 +1,20 @@
 
     <section>
         <h3>News</h3>
+        <p>Bienvenue sur la page des news</p>
+        <p>Et maintenant ?!</p>
+        <div class="ligne x3col">
+            <div>
+                <h4>titre</h4>
+                <img src="assets/images/photo.jpg" alt="photo">
+            </div>
+            <div>
+                <h4>titre</h4>
+                <img src="assets/images/photo.jpg" alt="photo">
+            </div>
+            <div>
+                <h4>titre</h4>
+                <img src="assets/images/photo.jpg" alt="photo">
+            </div>
+        </div>
     </section>
