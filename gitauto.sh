a31d097ff2e10a080b32be488f4cdddd6037fb2b
workodin <hello@workodin.com>
2020-01-21 02:59:47 +0100

    gitauto.sh

--- gitauto.sh
+++ gitauto.sh
@@ -10,10 +10,6 @@ if [ -z "$listmodif" ]
 then
     listfile=`git status --porcelain | cut -c4-`
 
-    for f in "$listfile"
-    do
-        echo $f
-    done
     git add -A 
     git commit -a -m "$listfile"
     git push origin master
